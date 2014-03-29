90f09259f624dd48411e47b7f5097df0af879fcd
koppepanyaro <koppepan11@gmail.com>
2014-03-29 21:41:11 +0900

    MyFizzBuzz failer commit

--- FizzBuzz/FizzBuzzTest.java
+++ FizzBuzz/FizzBuzzTest.java
@@ -6,14 +6,19 @@ public class FizzBuzzTest extends TestCase {
 	}
 
 	public void test() throws Exception {
+
 		FizzBuzz myFizzBuzz = new MyFizzBuzz();
 
-		String fizz = myFizzBuzz.calc(3);
-		String buzz = myFizzBuzz.calc(5);
-		String fizzbuzz = myFizzBuzz.calc(15);
+//		String fizz = myFizzBuzz.calc(3);
+//		String buzz = myFizzBuzz.calc(5);
+//		String fizzbuzz = myFizzBuzz.calc(15);
+//		String other = myFizzBuzz.calc(11);
 
-		assertEquals("Fizz", fizz);
-		assertEquals("Buzz", buzz);
-		assertEquals("Fizz Buzz", fizzbuzz);
+		for(int i = 0 ; i < 20 ; i++){
+			assertEquals("Fizz", i);
+			assertEquals("Buzz", i);
+			assertEquals("Fizz Buzz", i);
+			assertEquals("11", i);
+		}
 	}
 }
--- FizzBuzz/MyFizzBuzz.java
+++ FizzBuzz/MyFizzBuzz.java
@@ -1,5 +1,13 @@
 public class MyFizzBuzz implements FizzBuzz {
 	public String calc(int i) {
-		return "AAA";
+		if(i %15 == 0){
+			return "Fizz Buzz";
+		}else if(i % 5 == 0){
+			return "Buzz";
+		}else if(i % 3 == 0){
+			return "Fizz";
+		}else{
+			return Integer.toString(i);
+		}
 	}
 }
