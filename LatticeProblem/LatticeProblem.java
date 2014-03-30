ef8cd8bc437caef551259196938b9e651d187408
uyaponz <uyaponz@gmail.com>
2014-03-30 17:35:00 +0900

    implement LatticeProblem.getNumOfRoute() method

--- LatticeProblem/LatticeProblem.java
+++ LatticeProblem/LatticeProblem.java
@@ -1,5 +1,16 @@
+import java.util.Arrays;
+
 public class LatticeProblem {
 	public long getNumOfRoute(int x, int y) {
-		return 0;
+		long[] temporary = new long[x+1];
+		Arrays.fill(temporary, 1);
+
+		for (int yy=0; yy<y; yy++) {
+			for (int xx=1; xx<=x; xx++) {
+				temporary[xx] += temporary[xx-1];
+			}
+		}
+
+		return temporary[x];
 	}
 }
--- LatticeProblem/LatticeProblemTest.java
+++ LatticeProblem/LatticeProblemTest.java
@@ -8,7 +8,10 @@ public class LatticeProblemTest extends TestCase {
 	public void test() throws Exception {
 		LatticeProblem lp = new LatticeProblem();
 
-		// n_C_r : 40_C_20 = 137846528820
+		// 20x20 -> n_C_r:40_C_20 = 137846528820
 		assertEquals(137846528820L, lp.getNumOfRoute(20, 20));
+
+		// 5x4 -> n_C_r:9_C_4 = 126
+		assertEquals(126L, lp.getNumOfRoute(5, 4));
 	}
 }
